266314db389fd59e883fd6e265f504831a35e3b0
sunnyqltech <sunny@qltech.com.au>
2021-02-06 12:35:25 +0530

    commit 06-02 12:35

--- src/pages/about-asc/sponsors-and-partners.js
+++ src/pages/about-asc/sponsors-and-partners.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 import { Link } from 'gatsby'
-import Layout from "../components/layout"
+import Layout from "../../components/layout"
 
 import axios from 'axios';
 import {Container,Breadcrumb} from 'react-bootstrap'
