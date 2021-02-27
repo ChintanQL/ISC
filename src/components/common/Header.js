3c0aaface67a56023a9b6adb478d5491052ee272
sunnyqltech <sunny@qltech.com.au>
2021-02-27 14:31:53 +0530

    commit 27-02 14:30

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -68,7 +68,7 @@ class Header extends Component {
 							
 							<li className="userdropdownbox">
                             <i className="fa fa-user-circle"></i>{cookies.get('USER')}
-                            <ul className="htdropdown" style={{ display: show ? "block" : "none" }} onClick={()=>{this.setState({show:!this.state.show})}} >
+                            <ul className="htdropdown" style={{ display: this.state.show ? "block" : "none" }} onClick={()=>{this.setState({show:!this.state.show})}} >
                                 <li><a href="https://shop.australiansportscamps.com.au/my-account/">Profile</a></li>
                                 <li><a href={cookies.get('L_URL')}>logout</a></li>
                             </ul>
