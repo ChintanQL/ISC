678bc4b06906b5a6475d5ee247c8ca330c8c561a
sunnyqltech <sunny@qltech.com.au>
2021-03-04 14:03:41 +0530

    commit 04-03 14:05

--- src/pages/school-holiday-programs.js
+++ src/pages/school-holiday-programs.js
@@ -88,7 +88,7 @@ class SHP extends Component {
 					</Container>
 				</section>
 				
-				<section className="Sport-section-3">
+				<section className="Sport-section-3 mt-5">
 					<Container>
 						<Row>
 							{(this.state.showInfo === 1) ? (
