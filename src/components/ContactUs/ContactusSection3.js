027f7dd7a2febb58330550e31723a4e09ee74848
sunnyqltech <sunny@qltech.com.au>
2021-04-12 15:40:29 +0530

    commit 12-04 15:40

--- src/components/ContactUs/ContactusSection3.js
+++ src/components/ContactUs/ContactusSection3.js
@@ -3,20 +3,26 @@ import {Container,Form,Row,Col,Button} from 'react-bootstrap'
 import $ from "jquery";
 
 class ContactusSection3 extends Component {
+	constructor(props) {
+      
+        this.resizeIFrameToFitContent = this.resizeIFrameToFitContent.bind(this); 
+    }
 	
-	componentDidMount() {
-		$("#frame").load(function() {
-			$(this).height( $(this).contents().find("body").height() );
-		});
+	resizeIFrameToFitContent( ) {
+		var iFrame = document.getElementById( 'frame' );
+		//iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
+		iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
 	}
-	
+	hideSpinner = () => {
+		
+	  };
 	
     render() {      
         return (
             <>
             <section className="Contactus-Section-3">
             <div className="container">
-                <iframe id="frame"  width="100%" frameBorder="0" src="https://shop.australiansportscamps.com.au/gravity-contact-us/" title="description" /> 
+                <iframe id="frame"  width="100%" onLoad={this.resizeIFrameToFitContent} frameBorder="0" src="https://shop.australiansportscamps.com.au/gravity-contact-us/" title="description" /> 
             </div>
             </section>
             </>
