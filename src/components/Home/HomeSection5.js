f1263dc4369c72dcf67ed3d0ef145bab72e6bc2a
sunnyqltech <77315801+sunnyqltech@users.noreply.github.com>
2021-08-05 09:51:31 +0530

    Update HomeSection5.js
    Mobile Banner Updates

--- src/components/Home/HomeSection5.js
+++ src/components/Home/HomeSection5.js
@@ -5,26 +5,33 @@ import trainer1 from '../../images/trainer1.png'
 import trainer3 from '../../images/trainer3.png'
 import trainer4 from '../../images/trainer4.png'
 import axios from 'axios';
-
+import {isMobile} from 'react-device-detect';
 
 class HomeSection5 extends Component {
     
 	constructor(props) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
+    		super(props);
+    		this.handleLoginClick = this.handleLoginClick.bind(this);
+		this.handleLoginClick2 = this.handleLoginClick2.bind(this);
    
-  }
+  	}
 
 	handleLoginClick() {
     this.setState({Isbanner: 0});
   }
-	
+handleLoginClick2() {
+    this.setState({MIsbanner: 0});
+  }	
 	
 	state = {
 		PageData: [],
 		Isbanner: 0,
 		coupon_code: "",
-		showInfo: 0
+		showInfo: 0,
+		MPageData: [],
+		MIsbanner: 0,
+		Mcoupon_code: "",
+		MshowInfo: 0
 	}
 	
 	componentDidMount() {
@@ -36,19 +43,33 @@ class HomeSection5 extends Component {
 			this.setState({Isbanner: res.data.ResponseData[0].IsBanner})
 			this.setState({coupon_code: res.data.ResponseData[0].coupon_code})
 			this.setState({showInfo: 1})
+			this.setState({MPageData: res.data.ResponseData[1].Image})
+			this.setState({MIsbanner: res.data.ResponseData[1].IsBanner})
+			this.setState({Mcoupon_code: res.data.ResponseData[1].coupon_code})
+			this.setState({MshowInfo: 1})
 		})
 	}
 	
-	
-	render() {      
-        return (
-            <>
+	renderContent = () => {
+    if (!isMobile) {
+        return  <>
            <div className="sticky-footer" style={{ display: this.state.Isbanner == 0 ? "none" : "block" }} >
 			<button className="closebtn" onClick={this.handleLoginClick} >x</button>
 			<Link to={"/book-a-camp/"+this.state.coupon_code} ><Image src={this.state.PageData} alt=""/></Link>
 		   </div>
             </>
-        );
+    }
+    return  <>
+            <div className="popupcenter_open">
+		   <div className="popup_cntbx" style={{ display: this.state.MIsbanner == 0 ? "none" : "block" }} >
+   
+    <div className="popupimage"> <button type="button" className="close" onClick={this.handleLoginClick2}>&times;</button><Link to={"/book-a-camp/"+this.state.Mcoupon_code} ><img src={this.state.MPageData} alt="" className="img-fluid"/></Link></div></div>
+  </div>
+            </>
+}
+	
+	render() {      
+        return this.renderContent();
     }
 }
 
