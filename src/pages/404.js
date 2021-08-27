5bc57ab0e1daf4c32bbf9c2633d944bf98479dec
sunnyqltech <77315801+sunnyqltech@users.noreply.github.com>
2021-08-27 14:10:10 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from "react"
 import {Container,Breadcrumb,Row,Col} from 'react-bootstrap'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import fri_image from "../images/cricket-camp-friends.jpg.jpg"
 import { Link } from 'gatsby'
 import { Helmet } from "react-helmet"
 const NotFoundPage = () => (
@@ -22,18 +23,26 @@ const NotFoundPage = () => (
 			</div>
 		</Container>
 	</section>
-	<section className="Blog-Section-2">
-		<Container>
-			<div className="Blog-Section-2-data">
-				<Row>
-					<Col xl={12} lg={12} md={12} className="lg-mb-2">
-						<h1>404: Not Found</h1>
-						<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-					</Col>
-				</Row>
-			</div>
-		</Container>
-	</section>
+	<section class="thank-you-Section-1">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-xl-8 col-lg-8 lg-mb-8">
+                    <div class="thank-you-data-one text-center">
+						
+						 <h2 class="font-bold color-blue uppercase">Whoops!</h2>
+						 <p class="font-20 font-medium">We couldn’t find the page you’re looking for</p>
+                        <div class="thankyoumain_img"><img src={fri_image} alt=""/></div>
+                       
+                        <p class="font-20 font-medium">Looks like this page went to attend one of our camps
+Try our homepage or resources instead</p>
+                        
+
+                        
+                    </div>
+                </div>
+                </div>
+                </div>
+                </section>
 	
 	<SEO title="404: Not found" />
    
