20844e499009c1209d44a0d76ec464c36c33b31d
sunnyqltech <77315801+sunnyqltech@users.noreply.github.com>
2021-09-29 09:46:12 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-sass',
+'gatsby-plugin-robots-txt', 
 	{
         resolve: `gatsby-plugin-advanced-sitemap`,
         options: {
