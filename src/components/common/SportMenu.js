85e3b8fb8d679a09a959e1aca5e81af2ad8ded56
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2021-10-27 11:43:52 +0530

    Update SportMenu.js

--- src/components/common/SportMenu.js
+++ src/components/common/SportMenu.js
@@ -68,6 +68,10 @@ class SportMenu extends Component {
 			)}
 		  />
 		  </ListGroup>
+<div className="mb-3 menu-heading">
+					
+					<a className="" href="/camps/asc-explorers">Sport Explorers (For 3-5 year olds)</a>
+				</div>
 		  </div>
 		  <SportMenu2/>
             </>
