c58fb66e7fc0818a39231e20480574a3d21ebdd8
Chintan Pandya <chintanapandya@gmail.com>
2022-04-28 12:39:43 +0530

    ui changes 6

--- src/components/ContactUs/ContactusSection3.js
+++ src/components/ContactUs/ContactusSection3.js
@@ -7,9 +7,17 @@ class ContactusSection3 extends Component {
     render() {      
         return (
             <>
-            <section className="Contactus-Section-3">
+            <section class="cont_form">
             <div className="container">
+			<div class="row">
+					<div class="col-md-12">
+						<div class="our_fomr">
+							<div class="fomr_cong pb">
                 <iframe height="1150" width="100%" frameBorder="0" src="https://shop.spinhouse4u.com/gravity-contact-us/" title="description" /> 
+            </div>
+            </div>
+            </div>
+            </div>
             </div>
             </section>
             </>
