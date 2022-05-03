586e58b7d706d479b8209a7f1d6712fb5c9f9130
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-05-03 10:58:41 +0530

    Update Mmenu.js

--- src/components/common/Mmenu.js
+++ src/components/common/Mmenu.js
@@ -79,7 +79,7 @@ class Mmenu extends Component {
 									{(prop.title == 'Resources') ? (<>
 										<li className="menu-item-has-children d-none" id="dmenut" >
 										<span class="menu-expand"><i></i></span>
-										<Link className="mm" dataid={prop.id} data-toggle={prop.id}
+										<Link className="mm" dataid={prop.id} data-toggle={"tab"+prop.id}
 												href="javascript:;" aria-expanded="false">{prop.title}</Link>
 											<ul className={"dropdown drr d-none dr"+prop.id} id="dmenu" >
 												<li  key={i} ><Link className="dropdown-item"   to={"/resources"}>All</Link></li>
@@ -97,7 +97,7 @@ class Mmenu extends Component {
 									</>) : (<>
 										<li className={(prop.title == 'Contact') ? ("menu-item-has-children") : ("menu-item-has-children")}    id="d3menut" >
 										<span class="menu-expand"><i></i></span>
-										<Link className="mm"  dataid={prop.id}  data-toggle={prop.id}
+										<Link className="mm"  dataid={prop.id}  data-toggle={"tab"+prop.id}
 												href="javascript:;" aria-expanded="false">{prop.title}</Link>
 											<ul className={"dropdown drr d-none dr"+prop.id} id="d3menu">
 												{prop && prop.child_items && prop.child_items.map((child, i) => {
