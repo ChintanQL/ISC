0b5d84b3fb3beca42cb336985a2bd909c713d3d9
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-05-13 18:38:17 +0530

    Update F2Menu.js

--- src/components/common/F2Menu.js
+++ src/components/common/F2Menu.js
@@ -54,7 +54,7 @@ class F2Menu extends Component {
 									(<Link to={"/about-asc/sponsors-and-partners"} className="" dangerouslySetInnerHTML={{ __html: innerprop.title}} />) : 
 									(
 										<>{(innerprop.title.replace(/\s+/g, '-').toLowerCase() == 'work-with-us') ? (<Link to={"/asc-coaches-staff-enquiry"} className="" dangerouslySetInnerHTML={{ __html: innerprop.title}} />) : (
-										<>{(innerprop.title.toLowerCase() == 'terms & conditions') ? (<Link to={"/'terms-condition"} className="" dangerouslySetInnerHTML={{ __html: innerprop.title}} />) : (
+										<>{(innerprop.title.toLowerCase() == 'terms & conditions') ? (<Link to={"/'terms-conditions"} className="" dangerouslySetInnerHTML={{ __html: innerprop.title}} />) : (
 										<>{(innerprop.title.replace(/\s+/g, '-').toLowerCase() == 'asc-heroes') ? (<Link to={"/the-asc-heroes"} className="" dangerouslySetInnerHTML={{ __html: innerprop.title}} />) : (
 										
 										<>{(innerprop.title.replace(/\s+/g, '-').toLowerCase() == 'forms-for-coaches-&-staff') ? (<Link to={"/asc-coaches-staff"
