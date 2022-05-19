83079d07105d51ad9d83963e320f75b7799dadbb
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-05-19 14:37:25 +0530

    Create sorry.js

---
+++ src/pages/sorry.js
@@ -0,0 +1,36 @@
+import React from "react"
+import {Container,Breadcrumb,Row,Col} from 'react-bootstrap'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import fri_image from "../images/kids.png"
+import { Link } from 'gatsby'
+import { Helmet } from "react-helmet"
+const FoundPage = () => (
+  
+  <Helmet>
+				<title>Sorry</title>
+			</Helmet>
+    
+	<section className="thank-you-Section-1">
+        <div className="container">
+            <div className="row align-items-center justify-content-center">
+                <div className="col-xl-8 col-lg-8 lg-mb-8">
+                    <div className="thank-you-data-one text-center">
+						
+						 <h2 className="font-bold color-blue uppercase">Sorry!</h2>
+						
+                        
+
+                        
+                    </div>
+                </div>
+                </div>
+                </div>
+                </section>
+	
+	
+   
+
+)
+
+export default FoundPage
